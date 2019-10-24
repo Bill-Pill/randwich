f20a8892d485138146dd9b4c8a442e1c8a619fc1
Jenesis Bonitto <user@Users-MacBook-Air.local>
2019-10-24 12:43:42 -0400

    installed and imported axios

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import 'bootstrap/dist/css/bootstrap.css';
+import axios from "axios"
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render( < App / > , document.getElementById('root'));
