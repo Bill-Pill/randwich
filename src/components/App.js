12b21225e5bf4cce763dae466aa14340deaad523
Jenesis Bonitto <user@Users-MacBook-Air.local>
2019-10-24 09:10:42 -0400

    first commit, testing button, click event and geolocation

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>
-          #RandwichLife
-        </h1>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  // This syntax ensures `this` is bound within handleClick.
+  // Warning: this is *experimental* syntax.
+  handleClick = () => {
+    console.log('this is:', this);
+  }
+
+  render() {
+    return (
+      <button variant="warning" size="lg"  onClick={this.handleClick}>
+        Randwich
+      </button>
+    );
+  }
 }
 
 export default App;
