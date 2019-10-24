d259e68cb0e38c863f3c62fb5eaa16b1a745b0cc
Billy DeLashmutt <delashmuttwa@gmail.com>
2019-10-24 10:14:14 -0400

    Add fetchCurrentCoords functionality. WIP.

--- src/components/App.js
+++ src/components/App.js
@@ -2,20 +2,49 @@ import React from 'react';
 
 
 class App extends React.Component {
-    consructor() {
+    constructor() {
     super()
 
     this.state = {
       currentCoords: {
-        latitude: null,
-        longitude: null
+        latCoord: null,
+        longCoord: null
       }
+    }
   }
-}
+  // When invoked, grabs current latitude and longitude
+  // coordinates of user and saves to state
+  fetchCurrentCoords = () => {
+
+    // Geolocation parameters ensures a fresh location pull
+    // and stops trying to fetch if longer than 5 seconds
+    const options = {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0
+    };
+
+    // On successful fetch, saves coords.
+    const success = (position) => {
+      const crd = position.coords;
+      console.log('Your current position is:');
+      console.log(`Latitude : ${crd.latitude}`);
+      console.log(`Longitude: ${crd.longitude}`);
+      console.log(`More or less ${crd.accuracy} meters.`);
+    }
+
+    // If failed to fetch location, log errors in console.
+    const error = (err) => {
+      console.warn(`ERROR(${err.code}): ${err.message}`);
+    }
+    navigator.geolocation.getCurrentPosition(success, error, options);
+  }
+
   // This syntax ensures `this` is bound within handleClick.
   // Warning: this is *experimental* syntax.
   handleClick = () => {
     console.log('this is:', this);
+    this.fetchCurrentCoords()
   }
 
   render() {
