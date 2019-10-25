ec78b7cfa14103f957c313acf913d315d1f5b88c
Billy DeLashmutt <delashmuttwa@vt.edu>
2019-10-24 20:22:11 -0400

    Add name, phone, price, and address to SandwichPlace component

--- src/components/SandwichPlace.js
+++ src/components/SandwichPlace.js
@@ -5,8 +5,14 @@ class SandwichPlace extends React.Component {
   render() {
     const placeDetailsObj = this.props.place
     return (
-      <img className="mainSandwich" src={placeDetailsObj.image}></img>
-      // <img className="mainSandwich" src="https://s3-media3.fl.yelpcdn.com/bphoto/SiRfp5l__BN1zDV8BLEV7Q/o.jpg"></img>
+      <div>
+        <img className="mainSandwich" src={placeDetailsObj.image}></img>
+        <p>Name: {placeDetailsObj.name}</p>
+        <p>Phone: {placeDetailsObj.phone}</p>
+        <p>Price: {placeDetailsObj.price}</p>
+        <p>Address: {placeDetailsObj.address}</p>
+      </div>
+      
     );
   }
 }
