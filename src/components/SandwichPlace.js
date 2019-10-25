799f4b52513d56aea6f39d2b7f063ae461f8a110
Jenesis Bonitto <user@Users-MacBook-Air.local>
2019-10-25 11:42:55 -0400

    styling updated for sandwich place component. completed,may add get directions

--- src/components/SandwichPlace.js
+++ src/components/SandwichPlace.js
@@ -6,36 +6,24 @@ class SandwichPlace extends React.Component {
     const placeDetailsObj = this.props.place
     return (
 
-      <div className="card mb-3" style={{ maxWidth: '540px' }}>
-        <div className="row no-gutters">
-          <div className="col-md-4">
-            <img src={placeDetailsObj.image} className="card-img"/>
-          </div>
-          <div className="col-md-8">
-            <div className="card-body">
-              <h5 className="card-title" >{placeDetailsObj.name}</h5>
-              <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-              <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
-            </div>
-          </div>
+      <div>
+      <div className="card">
+        <img src={placeDetailsObj.image} className="card-img-top"/>
+      </div>
+        <div className="card-body">
+          <h5 className="card-title" >{placeDetailsObj.name}</h5>
+          <p className="card-text card-price">Price: <span>{placeDetailsObj.price}</span></p>
+          <p className="card-text">Phone: {placeDetailsObj.phone}</p>
+          <p className="card-text">Address: {placeDetailsObj.address}</p>
+          <p className="card-text">City: {placeDetailsObj.city}</p>
+          <p className="card-text">Zip: {placeDetailsObj.zip}</p>
+          <p className="card-text">Distance: {placeDetailsObj.distance} miles</p>
+          <p className="card-text"><small className="text-muted">Mouth not watering? Click again for another location!</small></p>
         </div>
       </div>
     );
   }
 };
-//       <div>
-//         <img className="mainSandwich" src={placeDetailsObj.image}></img>
-//         <p>Name: {placeDetailsObj.name}</p>
-//         <p>Phone: {placeDetailsObj.phone}</p>
-//         <p>Price: {placeDetailsObj.price}</p>
-//         <p>Address: {placeDetailsObj.address}</p>
-//         <p>City: {placeDetailsObj.city}</p>
-//         <p>Zip: {placeDetailsObj.zip}</p>
-//         <p>Distance: {placeDetailsObj.distance} miles</p>
-//       </div>
-      
-//     );
-//   }
-// }
+
 
 export default SandwichPlace;
